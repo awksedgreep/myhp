d3dc07f57f6e5055bfd48b1c04cbcae7884ef617
Mark Cotner <mark.cotner@gmail.com>
2025-07-18 00:48:33 -0400

    Add favicon.svg

--- config/runtime.exs
+++ config/runtime.exs
@@ -44,7 +44,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "mcotner.online"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :myhp, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
@@ -59,7 +59,11 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: [
+      "https://mcotner.online",
+      "https://myhp.fly.dev"
+    ]
 
   # ## SSL Support
   #
